33d34841c7712d0b4bb4c54bdf2297b4fc4b17d7
lzynfd <15196260302@163.com>
2019-01-24 13:48:45 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import {Button} from 'antd';
+import AppCss from './App.scss';
 class App extends Component {
+
+
+  componentDidMount(){
+    console.log('scss:',AppCss);
+  }
+
   render() {
     return (
-      <div >
+      <div>
+        <h3 className={AppCss.title}>ant design</h3>
+        <Button type="primary">ant design</Button>
       </div>
     );
   }
